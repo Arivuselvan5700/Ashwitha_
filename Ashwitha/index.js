20f517087e665f93cb4ed03b9b4e0dcff78eb9d3
Arivuselvan <arivuselvan96@gmail.com>
2020-04-09 23:23:10 +0530

    update

---
+++ Ashwitha/index.js
@@ -0,0 +1,14 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function openCat() {
+  document.getElementById("category").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
